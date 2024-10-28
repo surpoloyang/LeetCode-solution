09f77681bfcd4f4c55240440ffc9b9b2c198f990
CY637 <surpolotim420@gmail.com>
2024-10-28 22:36:39 +0800

    add 35搜索插入位置 and 883 三维形体投影面积

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
-        major = nums[0]
-        cnt = 1
-        for i in nums[1:]:
-            if i != major:
-                cnt -= 1
-                if cnt == 0:
-                    major = i
-            else:
-                cnt += 1
-        return major
-    
-Solution().majorityElement([3,2,3])
+    def projectionArea(self, grid: list[list[int]]) -> int:
+        xz, yz, xy = 0, 0, 0
+        yzmaxlist = {}
+        for i in range(len(grid)):
+            xz += max(grid[i])
+            for j in range(len(grid[i])):
+                if grid[i][j] != 0:
+                    xy += 1
+                yzmaxlist[j] = grid[i][j] if j not in yzmaxlist else max(yzmaxlist[j], grid[i][j])
+        yz = sum(yzmaxlist.values())
+        a = yz+xz+xy
+        return a
+Solution().projectionArea([[1,0],[0,2]])
